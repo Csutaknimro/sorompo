b43b30258465207a79069c1404a6e4af4789e8ab
Csutaknimro <nimrodcsutak@gmail.com>
2026-03-26 14:43:15 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -3,8 +3,20 @@ pins.digitalWritePin(DigitalPin.P16, 0)
 pins.digitalWritePin(DigitalPin.P15, 0)
 pins.digitalWritePin(DigitalPin.P13, 0)
 basic.forever(function () {
+	
+})
+basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P1, pins.analogReadPin(AnalogReadWritePin.P1))
-    if ((0 as any) < (500 as any)) {
-    	
+    if (pins.analogReadPin(AnalogReadWritePin.P1) < 500) {
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        while (pins.digitalReadPin(DigitalPin.P3) == 0) {
+            basic.pause(1000)
+        }
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
     }
 })
