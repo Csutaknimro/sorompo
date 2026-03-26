566b2cbc02a0e1d2309978418cd8228c17aa6f8f
Csutaknimro <nimrodcsutak@gmail.com>
2026-03-26 14:33:33 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+led.enable(false)
+pins.digitalWritePin(DigitalPin.P16, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+pins.digitalWritePin(DigitalPin.P13, 0)
 basic.forever(function () {
-	
+    pins.digitalWritePin(DigitalPin.P1, pins.analogReadPin(AnalogReadWritePin.P1))
+    if ((0 as any) < (500 as any)) {
+    	
+    }
 })
